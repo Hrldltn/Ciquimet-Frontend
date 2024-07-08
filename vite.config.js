f8c5d66c6d2a93be875d0c51a4f7fd16ed65f09f
hrldltn <haroldleytonol@gmail.com>
2024-07-08 00:29:24 -0400

    fix import envv3

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import commonjs from '@rollup/plugin-commonjs';
 
 // https://vitejs.dev/config/
