438fcdd385e2bfbd8f4b1a88afa56f85dd679926
hrldltn <haroldleytonol@gmail.com>
2024-07-08 02:06:16 -0400

    fix import envv3

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,  // Puedes cambiar a true si necesitas sourcemaps en producción
+  },
   server: {
-    host: '0.0.0.0', // Permite que el servidor escuche en todas las interfaces
-    port: process.env.PORT || 5173, // Usa el puerto de la variable de entorno PORT si está disponible, de lo contrario, usa el puerto 3000
+    host: '0.0.0.0',  // Permite que el servidor escuche en todas las interfaces
+    port: process.env.PORT || 5173,  // Usa el puerto de la variable de entorno PORT si está disponible, de lo contrario, usa el puerto 5173
   },
 });
