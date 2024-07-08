a16d413c773e2d1fa3f6b1596c5ae190a32335d4
hrldltn <haroldleytonol@gmail.com>
2024-07-08 00:36:46 -0400

    fix import envv3

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,11 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = import.meta.env;
-  const htmlPlugin = () => {
-  return {
-    resolve: {
-      alias: {
-        'react/jsx-runtime': 'react/jsx-runtime.js',
-      },
-    },
-    define: {
-      'process.env': process.env,
-    },
-    plugins: [htmlPlugin(), react({
-      babel: {
-        plugins: [
-          ['@babel/plugin-proposal-decorators', { legacy: true }],
-          ['@babel/plugin-proposal-class-properties', { loose: false }],
-          'babel-plugin-transform-typescript-metadata',
-          'babel-plugin-parameter-decorator',
-          ['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }],
-        ]
-      },
-    })],
-    
-    server: {
-      host: '0.0.0.0', // Permite que el servidor escuche en todas las interfaces
-      port: env.PORT || 5173, // Usa el puerto de la variable de entorno PORT si está disponible, de lo contrario, usa el puerto 3000
-    }
-   
-  };
-}
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Permite que el servidor escuche en todas las interfaces
+    port: process.env.PORT || 5173, // Usa el puerto de la variable de entorno PORT si está disponible, de lo contrario, usa el puerto 3000
+  },
 });
-  
