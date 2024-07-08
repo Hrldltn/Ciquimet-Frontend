269bdb96a960bd15e01b8f0e33b4f8edbe93bf13
hrldltn <haroldleytonol@gmail.com>
2024-07-07 22:42:18 -0400

    config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Permite que el servidor escuche en todas las interfaces
+    port: process.env.PORT || 5173, // Usa el puerto de la variable de entorno PORT si está disponible, de lo contrario, usa el puerto 3000
+  },
+});
